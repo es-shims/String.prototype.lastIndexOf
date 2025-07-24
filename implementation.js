93d3b326a6c4fdd7675fdd3003bcc2f836172f00
Jordan Harband <ljharb@gmail.com>
2025-07-24 09:39:12 -0500

    [Refactor] update algorithm to match latest spec
    Also includes https://github.com/tc39/ecma262/pull/3660

--- implementation.js
+++ implementation.js
@@ -1,33 +1,29 @@
 'use strict';
 
+var clamp = require('es-abstract/2025/clamp');
 var RequireObjectCoercible = require('es-object-atoms/RequireObjectCoercible');
+var StringLastIndexOf = require('es-abstract/2025/StringLastIndexOf');
 var ToIntegerOrInfinity = require('es-abstract/2025/ToIntegerOrInfinity');
 var ToNumber = require('es-abstract/2025/ToNumber');
 var ToString = require('es-abstract/2025/ToString');
-var clamp = require('es-abstract/2025/clamp');
 
 var $isNaN = require('es-abstract/helpers/isNaN');
-
-var callBound = require('call-bind/callBound');
-
-var $slice = callBound('String.prototype.slice');
 
 module.exports = function lastIndexOf(searchString) {
 	var O = RequireObjectCoercible(this); // step 1
 	var S = ToString(O); // step 2
 	var searchStr = ToString(searchString); // step 3
-	var numPos = ToNumber(arguments.length > 1 ? arguments[1] : NaN); // step 4
+	var numPos = arguments.length > 1 ? ToNumber(arguments[1]) : NaN; // step 4
 	var pos = $isNaN(numPos) ? Infinity : ToIntegerOrInfinity(numPos);
 
 	var len = S.length; // step 7
-	var start = clamp(pos, 0, len); // step 8
-	var searchLen = searchStr.length; // step 9
-
-	// 10. Let k be the largest possible non-negative integer not larger than start such that k + searchLen ≤ len, and for all non-negative integers j such that j < searchLen, the code unit at index k + j within S is the same as the code unit at index j within searchStr; but if there is no such integer, let k be -1.
-	for (var i = start; i >= 0; i -= 1) {
-		if ($slice(S, i, i + searchLen) === searchString) {
-			return i;
-		}
+	var searchLen = searchStr.length; // step 8
+	if (len < searchLen) {
+		return -1; // step 9
 	}
-	return -1;
+	var start = clamp(pos, 0, len - searchLen); // step 19
+
+	var result = StringLastIndexOf(S, searchStr, start); // step 11
+
+	return result === 'NOT-FOUND' ? -1 : result;
 };
